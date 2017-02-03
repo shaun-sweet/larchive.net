cc48d364b18b2b1e40b1a4391219eeabff0fcd6c
shaun <shaun@shaunsweet.com>
2017-02-03 02:34:43 -0700

    fixed fb callback url

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'facebookAuth' : {
         'clientID'      : '145337692637557', // your App ID
         'clientSecret'  : 'e80a4bb3e3fcc5711559003fe8b83aa3', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : '/auth/facebook/callback'
     }
   }
